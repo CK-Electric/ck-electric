fcd9e41cc418d1f949eff630c0687662bfaa9834
Erika <erikaaquinoenrique@gmail.com>
2026-03-08 19:16:36 -0300

    Improve SEO adding descriptive text to links

--- client/src/components/ServiceCard.tsx
+++ client/src/components/ServiceCard.tsx
@@ -35,6 +35,7 @@ export default function ServiceCard({
         icon={<ArrowRightAlt />}
         variant="tertiary"
         href={serviceLink}
+        ariaLabel={`Learn more about ${serviceTitle}`}
       />
     </div>
   );
